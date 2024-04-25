531f8769873c1a87b46b302f0015dba99527e2eb
HaitongC <haitongchen116@gmail.com>
2024-04-25 15:59:45 -0400

    plot summary

--- fanfic.js
+++ fanfic.js
@@ -16,3 +16,4 @@ document.getElementById("fanFicBtn").addEventListener("click", async () => {
         console.error("Error:", error);
     }
 });
+
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const API_KEY = window.myAppConfig.GOOGLE_API_KEY;
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+// confirm plot
 document.getElementById("plotBtn").addEventListener("click", function() {
     const userInput = document.getElementById("plotInput").value.trim();
     if (!userInput) {
@@ -8,4 +14,20 @@ document.getElementById("plotBtn").addEventListener("click", function() {
     const header = document.getElementById('mainHeader');
     header.textContent += " for ";
     header.textContent += userInput;
+
+    // generate summary of the plot
+    summarize(userInput);
 });
+
+async function summarize(userInput) {
+    const summary = document.getElementById("summary");
+    summary.textContent = "Hold tight! Generating a summary for " + userInput;
+    
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const prompt = "Summarize the show/book named " + userInput;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+
+    summary.textContent = text;
+  }
