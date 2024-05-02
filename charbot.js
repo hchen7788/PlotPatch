179854a24311234afac8107d71cd43a0bdc13e3f
HaitongC <haitongchen116@gmail.com>
2024-05-02 19:14:22 -0400

    chatbot steup

--- charbot.js
+++ charbot.js
@@ -3,6 +3,26 @@ import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/ge
 const API_KEY = window.myAppConfig.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+];
+const model = genAI.getGenerativeModel({ model: "gemini-pro", safetySettings});
+
 let plotName = localStorage.getItem('plotName');
 const header = document.getElementById('charBotHeader');
 header.textContent = "Character bot for plot for " + plotName;
@@ -10,38 +30,63 @@ header.textContent = "Character bot for plot for " + plotName;
 document.getElementById("charBotBtn").addEventListener("click", async () => {
     let char = document.getElementById("character").value.trim();
     document.getElementById("charBotContent").textContent = "Hold tight! Starting a conversation with " + char;
-    
-    try {
-        const safetySettings = [
-            {
-                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-              {
-                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-              {
-                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-              {
-                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-        ];
-        const model = genAI.getGenerativeModel({ model: "gemini-pro", safetySettings});
-
-        // const prompt = "Generate a fan fiction story for the work " + plotName + ", here are the main characters: " + char1 + "who is the " + role1 + ", and " + char2 + ", whoe is the " + role2 + ", and they are " + relationship + ". The backgroud is " + bg + ", and the story outline is " + story + ". Here are more details: " + more;
-        const prompt = "hey"
-        console.log(prompt);
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = await response.text();
-        console.log(text);
-        document.getElementById("charBotContent").textContent = text; // Display generated text in 'content' div
-    } catch (error) {
-        console.error("Error:", error);
-    }
 });
+
+
+async function getResponse(prompt){
+    const message = await model.generateContent(prompt);
+    const response = await message.response;
+    const text = response.text();
+
+    console.log(text);
+    return text;
+};
+
+// use chat div
+export const userDiv = (data) => {
+    return `
+    <!-- user chat -->
+            <div>
+                <p>${data}</p>
+            </div>
+    `;
+};
+
+// ai chat div
+export const aiDiv = (data) => {
+    return `
+    <!-- ai chat -->
+            <div>
+                <p>${data}</p>
+            </div>
+    `;
+};
+
+
+async function handleSubmit(event){
+    event.preventDefault();
+
+    let userMessage = document.getElementById("prompt");
+    const chatArea = document.getElementById("chat-container");
+
+    var prompt = userMessage.value.trim();
+    if (prompt === ''){
+        return;
+    }
+
+    chatArea.innerHTML += userDiv(prompt);
+    userMessage.value = "";
+
+    const aiResponse = await getResponse(prompt);
+    chatArea.innerHTML += aiDiv(aiResponse);
+}
+
+const chatFrom = document.getElementById("chat-form");
+chatFrom.addEventListener('submit', handleSubmit);
+
+chatFrom.addEventListener('keyup', (event)=>{
+    if (event.keyCode === 13) handleSubmit(event);
+})
+
+
 
