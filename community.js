d09c76a448b7df43eb19192f44460a4a2e8bf2a0
HaitongC <haitongchen116@gmail.com>
2024-05-02 22:02:28 -0400

    community done

--- community.js
+++ community.js
@@ -5,5 +5,13 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 let plotName = localStorage.getItem('plotName');
-const commheader = document.getElementById('communityHeader');
-commheader.textContent = "Find more about " + plotName;
+document.getElementById('communityHeader').textContent = "Find more about " + plotName;
+
+document.getElementById('social').textContent = "Social media activities on " + plotName;
+const prompt = "Find me some social media activities/groups about " + plotName;
+console.log(prompt);
+const result = await model.generateContent(prompt);
+const response = await result.response;
+const text = await response.text();
+console.log(text);
+document.getElementById("social").textContent += text; // Display generated text in 'content' div
